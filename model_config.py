adea16ccfa5f4897706be67e33a74be5ecd36ef7
ArviRizqi <85241896+ArviRizqi@users.noreply.github.com>
2025-06-16 20:39:00 +0700

    Update model_config.py

--- model_config.py
+++ model_config.py
@@ -1,36 +1,26 @@
-from tensorflow.keras import layers, models
-from tensorflow.keras.layers import Dense, Dropout, LayerNormalization, MultiHeadAttention
+# model_config.py
 import tensorflow as tf
+from tensorflow.keras import layers, models
 
 class TransformerBlock(layers.Layer):
-    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1, **kwargs):
-        super(TransformerBlock, self).__init__(**kwargs)
-        self.att = MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
+    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.5):
+        super(TransformerBlock, self).__init__()
+        self.att = layers.MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim // num_heads)
         self.ffn = models.Sequential([
-            Dense(ff_dim, activation="relu"),
-            Dense(embed_dim),
+            layers.Dense(ff_dim, activation="gelu"),
+            layers.Dense(embed_dim)
         ])
-        self.layernorm1 = LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = LayerNormalization(epsilon=1e-6)
-        self.dropout1 = Dropout(rate)
-        self.dropout2 = Dropout(rate)
+        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = layers.Dropout(rate)
+        self.dropout2 = layers.Dropout(rate)
 
     def call(self, inputs, training=None):
         inputs_expanded = tf.expand_dims(inputs, axis=1)
-        attn_output = self.att(inputs_expanded, inputs_expanded, training=training)
+        attn_output = self.att(inputs_expanded, inputs_expanded)
         attn_output = tf.squeeze(attn_output, axis=1)
         attn_output = self.dropout1(attn_output, training=training)
         out1 = self.layernorm1(inputs + attn_output)
         ffn_output = self.ffn(out1)
         ffn_output = self.dropout2(ffn_output, training=training)
         return self.layernorm2(out1 + ffn_output)
-
-    def get_config(self):
-        config = super().get_config()
-        config.update({
-            "embed_dim": self.att.key_dim,
-            "num_heads": self.att.num_heads,
-            "ff_dim": self.ffn.layers[0].units,
-            "rate": self.dropout1.rate,
-        })
-        return config
