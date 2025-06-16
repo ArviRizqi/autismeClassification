5ecd7dca1dec4e422cb4e57046abb8d7318850bc
ArviRizqi <85241896+ArviRizqi@users.noreply.github.com>
2025-06-16 20:49:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,13 @@
-# app.py
+from huggingface_hub import hf_hub_download
+from tensorflow.keras.models import load_model
 import streamlit as st
 import numpy as np
 from PIL import Image
-from tensorflow.keras.models import load_model
-from tensorflow.keras.layers import Dense, Dropout, LayerNormalization, MultiHeadAttention
-from huggingface_hub import hf_hub_download
+from model_config import TransformerBlock  # Impor custom layer
 
-from model_config import TransformerBlock  # Layer kustom
-
-# Konfigurasi custom_objects
-custom_objects = {
-    'TransformerBlock': TransformerBlock,
-    'Dense': Dense,
-    'Dropout': Dropout,
-    'LayerNormalization': LayerNormalization,
-    'MultiHeadAttention': MultiHeadAttention
-}
-
-# Load model dari HuggingFace
-model_path = hf_hub_download(repo_id="Artz-03/autismeClassification", filename="autisme-classifier.keras")
-
-st.title("🧠 Deteksi Autisme dari Gambar Wajah Anak")
-
-try:
-    model = load_model(model_path, custom_objects=custom_objects)
-    st.success("✅ Model berhasil dimuat.")
-except Exception as e:
-    st.error(f"❌ Gagal memuat model: {e}")
+# Load model dari Hugging Face
+model_path = hf_hub_download(repo_id="Artz-03/autismeClassification", filename="autism_hybrid_model.h5")
+model = load_model(model_path, custom_objects={'TransformerBlock': TransformerBlock})
 
 def preprocess_image(image):
     img = image.resize((224, 224))
@@ -34,16 +15,19 @@ def preprocess_image(image):
     img = np.expand_dims(img, axis=0)
     return img
 
+st.title("🧠 Deteksi Autisme dari Gambar Anak")
+
 uploaded_file = st.file_uploader("Upload gambar wajah anak", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file).convert("RGB")
+    image = Image.open(uploaded_file)
     st.image(image, caption="Gambar yang diunggah", use_container_width=True)
-    
     img_array = preprocess_image(image)
     prediction = model.predict(img_array)[0][0]
 
     if prediction <= 0.5:
-        st.error(f"🚨 Model memprediksi: **Autistik** (probabilitas: {1 - prediction:.2f})")
+        st.error(f"Model memprediksi: **Autistik** (probabilitas: {1 - prediction:.2f})")
     else:
-        st.success(f"✅ Model memprediksi: **Tidak Autistik** (probabilitas: {prediction:.2f})")
+        st.success(f"Model memprediksi: **Tidak Autistik** (probabilitas: {prediction:.2f})")
+
+
