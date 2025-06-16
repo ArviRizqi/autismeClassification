f39556929185743e527c8be1d47d437e1dab3975
ArviRizqi <85241896+ArviRizqi@users.noreply.github.com>
2025-06-16 20:41:37 +0700

    Update model_config.py

--- model_config.py
+++ model_config.py
@@ -2,25 +2,58 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
+from tensorflow.keras.layers import Layer, Dense
+import tensorflow as tf
+
+class CustomMultiHeadAttention(Layer):
+    def __init__(self, embed_dim, num_heads):
+        super(CustomMultiHeadAttention, self).__init__()
+        assert embed_dim % num_heads == 0
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.projection_dim = embed_dim // num_heads
+        self.query_dense = Dense(embed_dim)
+        self.key_dense = Dense(embed_dim)
+        self.value_dense = Dense(embed_dim)
+        self.combine_heads = Dense(embed_dim)
+
+    def attention(self, query, key, value):
+        score = tf.matmul(query, key, transpose_b=True)
+        dim_key = tf.cast(tf.shape(key)[-1], tf.float32)
+        scaled_score = score / tf.math.sqrt(dim_key)
+        weights = tf.nn.softmax(scaled_score, axis=-1)
+        return tf.matmul(weights, value)
+
+    def separate_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.projection_dim))
+        return tf.transpose(x, perm=[0, 2, 1, 3])
+
+    def call(self, inputs):
+        batch_size = tf.shape(inputs)[0]
+        query = self.separate_heads(self.query_dense(inputs), batch_size)
+        key = self.separate_heads(self.key_dense(inputs), batch_size)
+        value = self.separate_heads(self.value_dense(inputs), batch_size)
+        attention = self.attention(query, key, value)
+        attention = tf.transpose(attention, perm=[0, 2, 1, 3])
+        concat = tf.reshape(attention, (batch_size, -1, self.embed_dim))
+        return self.combine_heads(concat)
+
 class TransformerBlock(layers.Layer):
     def __init__(self, embed_dim, num_heads, ff_dim, rate=0.5):
-        super(TransformerBlock, self).__init__()
-        self.att = layers.MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim // num_heads)
+        super().__init__()
+        self.att = CustomMultiHeadAttention(embed_dim=embed_dim, num_heads=num_heads)
         self.ffn = models.Sequential([
             layers.Dense(ff_dim, activation="gelu"),
             layers.Dense(embed_dim)
         ])
-        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
-        self.dropout1 = layers.Dropout(rate)
-        self.dropout2 = layers.Dropout(rate)
+        self.norm1 = layers.LayerNormalization(epsilon=1e-6)
+        self.norm2 = layers.LayerNormalization(epsilon=1e-6)
+        self.drop1 = layers.Dropout(rate)
+        self.drop2 = layers.Dropout(rate)
 
     def call(self, inputs, training=None):
-        inputs_expanded = tf.expand_dims(inputs, axis=1)
-        attn_output = self.att(inputs_expanded, inputs_expanded)
-        attn_output = tf.squeeze(attn_output, axis=1)
-        attn_output = self.dropout1(attn_output, training=training)
-        out1 = self.layernorm1(inputs + attn_output)
+        attn_output = self.att(inputs)
+        out1 = self.norm1(inputs + self.drop1(attn_output, training=training))
         ffn_output = self.ffn(out1)
-        ffn_output = self.dropout2(ffn_output, training=training)
-        return self.layernorm2(out1 + ffn_output)
+        return self.norm2(out1 + self.drop2(ffn_output, training=training))
+
