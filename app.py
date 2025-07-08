a7b53b840cccb35f9a25781c52b9e510eb9ded5e
ArviRizqi <85241896+ArviRizqi@users.noreply.github.com>
2025-07-08 17:25:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ if uploaded_file:
         pred = torch.argmax(probs, dim=1).item()
         conf = probs[0, pred].item()
     
-        labels = {0: "Non-Autistic", 1: "Autistic"}
-        result = labels.get(pred, "Unknown")
+    labels = {0: "Non-Autistic", 1: "Autistic"}
+    result = labels.get(pred, "Unknown")
 
-        st.write(f"Prediction: **{result}** ({conf*100:.2f}%)")
+    st.write(f"Prediction: **{result}** ({conf*100:.2f}%)")
